5c99d947b0ffd5cbe8f36226d635ca01acbf563c
mohammadfahad5011 <mohammadfahad5011@gmail.com>
2023-02-08 21:14:04 +0600

    again change image path

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,8 +5,6 @@ const Hero = () => {
   return (
     <>
       <h1>Hi this is fahad</h1>
-      {/* <img src="/images/Apple.png" alt="apple" />
-      <img src="/images/hero.png" alt="Hero" /> */}
       <img src={hero} alt="Hero" />
     </>
   );
