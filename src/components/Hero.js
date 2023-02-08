7d5dd57c9a1a0204f116ac022a4bab94f510b3e0
mohammadfahad5011 <mohammadfahad5011@gmail.com>
2023-02-08 20:39:15 +0600

    add a new image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,6 +6,7 @@ const Hero = () => {
       <h1>Hi this is fahad</h1>
       <img src="/images/Apple.png" alt="apple" />
       <img src="/images/hero.png" alt="Hero" />
+      <img src="../../public/images/hero.png" alt="Hero" />
     </>
   );
 };
