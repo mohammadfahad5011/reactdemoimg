a3e82ce2be817bb58e6ef95b788a3cda76af635e
mohammadfahad5011 <mohammadfahad5011@gmail.com>
2023-02-08 10:50:09 +0600

    Initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <h1>Hi this is fahad</h1>
+      <img src="/images/Apple.png" alt="apple" />
+      <img src="/images/hero.png" alt="Hero" />
+    </>
+  );
+};
+
+export default Hero;
